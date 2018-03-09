7ceb98055975b6f205337157f0ab833ef302e954
Adrien Pyke <adpyke@gmail.com>
2018-03-08 23:50:16 -0500

    fix random function

--- client/js/lib/util.js
+++ client/js/lib/util.js
@@ -23,7 +23,7 @@
 		},
 		random(min, max) {
 			if (!max) [min, max] = [0, min];
-			return Math.floor(Math.random() * (max - min + 1)) + 1;
+			return Math.floor(Math.random() * (max - min + 1)) + min;
 		},
 		cookie: {
 			set(key, value, expiry) {

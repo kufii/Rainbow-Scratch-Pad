270f449af7d62a1a7a9c7d7fcff817468d64545a
Adrien Pyke <adpyke@gmail.com>
2018-03-17 15:39:54 -0400

    object.entries

--- client/js/module-loader.js
+++ client/js/module-loader.js
@@ -3,31 +3,33 @@
 
 	let app = window.app = window.app || {};
 
-	const addScript = function(src, cb) {
+	const addScript = function(src, cb, err) {
 		let script = document.createElement('script');
 		script.onload = cb;
+		script.onerror = err;
 		script.src = src;
 		document.head.appendChild(script);
 	};
 
 	app.loadModules = function(deps, done) {
-		let dir;
 		let loaded = 0;
 		let loading = 0;
 
-		const load = function() {
-			loaded += 1;
+		const load = () => {
+			loaded++;
 			if (loaded === loading) done();
 		};
 
-		for (dir in deps) {
-			if (deps.hasOwnProperty(dir)) {
-				deps[dir].forEach(file => {
-					let src = `${dir + file}.js`;
-					console.log(++loading, src);
-					addScript(src, load);
-				});
-			}
-		}
+		const error = err => {
+			console.error(`Error loading ${err.target.src}`);
+		};
+
+		Object.entries(deps).forEach(([key, value]) => {
+			value.forEach(file => {
+				let src = `${key + file}.js`;
+				console.log(++loading, src);
+				addScript(src, load, error);
+			});
+		});
 	};
 })();
